9909ea104a917571424de9ee1228aff47ed336fe
yusuf <majorhaji@github.com>
2023-03-29 12:25:04 +0100

    changed colour of tv guide and styling of tech stack, need to make tv guide functional

--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -1,16 +1,26 @@
+import { useEffect } from "react";
+
 const Channels = ({ setChannel }) => {
   const changeChannel = (channel) => {
     setChannel(channel);
+    console.log(channel);
   };
 
   return (
     <aside className="channel">
       <h1>TV GUIDE</h1>
       <div className="station" id="0" onClick={changeChannel(0)}>
-        <h4>Channel 0</h4>
+        <h4>Channel 1</h4>
         <p>
           A stock portfolio web app that lets users track stock holdings over
-          time and read news
+          time and read news with AI stock predictions and login/logout.
+        </p>
+      </div>
+      <div className="station" id="1" onClick={changeChannel(1)}>
+        <h4>Channel 2</h4>
+        <p>
+          A news website similar to reddit with topics, articles, comments and
+          persistent upvote/downvote. Can add, delete and update.{" "}
         </p>
       </div>
     </aside>
